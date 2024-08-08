c85e9702c1fe24f53472ad53609476b9677a96cc
farzin <farzinparakkal123@gmail.com>
2024-08-08 09:10:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("This is basic javascript to know the var,let and const");
+
+
+var a="I am jacob"
+var b=10
+console.log ("This is a string variable ",a," The b variable is ",b);
+console.log("type a = ",typeof(a)," b = ",typeof(b));
+
+let la=10;
+console.log("a:", la);
+
+const c="st";
+console.log("let variable a:", c);
